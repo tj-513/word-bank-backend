ea8146d9714738619fbbcaf512f6769ef7d5aafb
tj513 <tranaweera755@gmail.com>
2020-07-06 02:16:11 +0530

    run docker in background

--- infra/build.sh
+++ infra/build.sh
@@ -2,4 +2,4 @@
 
 docker build -t word-bank -f Dockerfile .
 docker rm --force word-bank-container
-docker run --publish 3000:3000 --name word-bank-container word-bank:latest
+docker run --publish 3000:3000 --detach --name word-bank-container word-bank:latest
