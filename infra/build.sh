21d626b2a22fd09ab7cbf719167a462630337067
tj513 <tranaweera755@gmail.com>
2020-07-06 02:05:43 +0530

    update docker file

--- infra/build.sh
+++ infra/build.sh
@@ -2,4 +2,4 @@
 
 docker build -t word-bank -f Dockerfile .
 docker rm --force word-bank-container
-docker run --publish 8000:8080 --detach --name word-bank-container word-bank
+docker run --publish 8000:8080 --detach --name word-bank-container word-bank:latest
