90dc47eddb042656f2dbafb367c243d26805a3a4
tj513 <tranaweera755@gmail.com>
2020-07-06 01:51:41 +0530

    make docker file run

--- infra/build.sh
+++ infra/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t word-bank -f Dockerfile .
+docker build -t word-bank -f Dockerfile
+docker run --publish 8000:8080 --detach --name word-bank-container word-bank
